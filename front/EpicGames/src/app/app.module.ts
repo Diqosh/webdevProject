59ec8bd40ae987175f17d4750345054f510301cb
Diqosh <d_tleuzhanuly@kbtu.kz>
2022-04-05 22:23:22 +0600

    verstka

--- front/EpicGames/src/app/app.module.ts
+++ front/EpicGames/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PrimaryNavigationComponent } from './primary-navigation/primary-navigation.component';
+import { ProductsWrapperComponent } from './products-wrapper/products-wrapper.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrimaryNavigationComponent,
+    ProductsWrapperComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
