ae1471b7c3863f7540e1308e4d3a513eef02d1e8
Yerlam <erlan200225@gmail.com>
2022-05-04 16:57:07 +0600

    back

--- back/api/urls.py
+++ back/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from api.views import productList, categoryList, categoryDetailAPIViews, whislistList, wishlist_del
+from api.views import productList, categoryList, categoryDetailAPIViews, whislistList, wishlist_del, productListAPIViews
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('products/', productList),
+    path('products/', productListAPIViews.as_view()),
     path('products/<int:id>', categoryDetailAPIViews.as_view()),
     path('whishlist/<int:user_id>', whislistList),
     path('whishlist/<int:user_id>/<int:product_id>', wishlist_del),
